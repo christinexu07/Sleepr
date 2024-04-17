73d83bf53e8cc74c2903592fa3d08250654680ec
christinexu07 <christinexu0.7@gmail.com>
2024-04-17 11:01:19 -0700

    just need to fix recycler view

---
+++ app/src/main/java/com/mistershorr/loginandregistration/Constants.kt
@@ -0,0 +1,6 @@
+package com.mistershorr.loginandregistration
+
+object Constants {
+    const val API_KEY = "D8D77B05-CE60-45D5-A0B0-B869169A8465"
+    const val APP_ID = "D1057F0E-06A5-6264-FF84-52B90D4E3200"
+}
